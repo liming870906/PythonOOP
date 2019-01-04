975379cdb8d88dfebf4fd53e82b07155be1398e3
liming870906 <370322364@qq.com>
2019-01-04 10:27:35 +0800

    私有属性；成员特殊方法

--- oop_test_demo/D002_Inherit.py
+++ oop_test_demo/D002_Inherit.py
@@ -3,28 +3,153 @@
 """
 
 
-# 基类（父类）
-class Father:
+# # 基类（父类）
+# class Father:
+#
+#     def f1(self):
+#         print('F.f1')
+#
+#     def f2(self):
+#         print('F.f2')
+# # 派生类（子类）
+# class Son(Father):
+#
+#     def s1(self):
+#         print('S.s1')
+#
+#     def f1(self):
+#         print('S.f1')
+#         super(Son,self).f1()
+#
+#     def f2(self):
+#         print('S.f2')
+#         Father.f2(self)
+#
+#
+# if __name__ == '__main__':
+#     son = Son()
+#     son.s1()
+#     son.f1()
+#     son.f2()
+#     son.f2()
+#     print('分隔符'.center(100,'-'))
+#     father = Father()
+#     father.f1()
+#     father.f2()
+#     # father.s1()
 
-    def f1(self):
-        print('F.f1')
 
-    def f2(self):
-        print('F.f2')
-# 派生类（子类）
-class Son(Father):
 
-    def s1(self):
-        print('S.s1')
+# class Base:
+#     def a(self):
+#         print("base.a")
+#
+# class GF(Base) :
+#     def a1(self):
+#         print('GF.a')
+# class F1(GF) :
+#     def a1(self):
+#         print('f1.a')
+#         super(F1,self).a()
+# class F2(Base) :
+#     def a(self):
+#         print('f2.a')
+#
+# class S(F1, F2):
+#     pass
+#
+# son = S()
+# son.a()
 
+# class BaseRequest:
+#     def __init__(self):
+#         print('BaseRequest-init')
+# class RequestHandler:
+#
+#     def __init__(self):
+#         print('RequestHandler-init')
+#         BaseRequest.__init__(self)
+#
+#     def serve_forever(self):
+#         print('RequestHandler-serve_forever')
+#         self.process_request()
+#
+#     def process_request(self):
+#         print('RequestHandler-process-request')
+#
+# class Minx:
+#
+#     def process_request(self):
+#         print('Minx-process-request')
+#
+# class Son(Minx, RequestHandler):
+#
+#     @staticmethod
+#     def abc():
+#         print('static method')
+#
+#     @classmethod
+#     def bcd(cls):
+#         # cls 是类名
+#         print('class method')
+#
+#
+#     @staticmethod
+#     def cde():
+#         pass
+#
+# s = Son()
+#
+# s.serve_forever()
+#
+# Son.abc()
+# Son.bcd()
+
+
+
+# import socketserver
+#
+# obj = socketserver.ThreadingTCPServer(1,2)
+# obj.serve_forever()
+
+class PageObject:
+    def __init__(self, p):
+        try:
+            self.page = int(p)
+        except Exception as ex:
+            self.page = 1
+        self.number = 0
+    @property
+    def start(self):
+        return (self.page - 1) * 10
+    @property
+    def end(self):
+        return self.page * 10
+
+    def getA(self):
+        return self.number
+    def setA(self,num):
+        self.number = num
+    def delA(self):
+        self.number = 0
+
+    pro = property(fget=getA,fset=setA,fdel=delA)
 
 if __name__ == '__main__':
-    son = Son()
-    son.s1()
-    son.f1()
-    son.f2()
-    print('分隔符'.center(100,'-'))
-    father = Father()
-    father.f1()
-    father.f2()
-    # father.s1()
+    li = []
+    for i in range(200):
+        li.append(i)
+
+    while True:
+        page = input('please print page number:')
+        mPage = PageObject(page)
+        print(li[mPage.start:mPage.end])
+        if li:
+            mPage.pro = li[int(mPage.start + (mPage.end - mPage.start)/2)]
+            print(mPage.pro)
+            del mPage.pro
+            print(mPage.pro)
+
+
+
+
---
+++ oop_test_demo/D003_Member_Property.py
@@ -0,0 +1,84 @@
+# 私有修饰符："__"
+# 子类不可以访问父类的自由字段
+"""
+
+class BaseFoo :
+    def __init__(self):
+        self.__gene = 987
+
+class Foo(BaseFoo):
+    __v = "123"
+    def __init__(self,name, age):
+        self.name = name
+        # 私有属性
+        self.__age = age
+        super(Foo,self).__init__()
+
+    def showAge(self):
+        print(self.__age)
+
+    @staticmethod
+    def showV():
+        return Foo.__v
+
+    def __getMe(self):
+        return '456'
+    def getM(self):
+        return self.__getMe()
+
+    def showPrint(self):
+        print(self.name)
+        print(self.__age)
+        # print(self.__gene)
+
+foo = Foo("benben",20)
+print(foo.name)
+foo.showAge()
+print(Foo.showV())
+print(foo.getM())
+foo.showPrint()
+
+"""
+
+# 特殊成员
+"""
+__doc__ 查看尖的描述信息
+__module__表示当前操作的对象所在的模块
+__class__表示当前操作的对象所属的类
+__init__构造方法 通过类创建对象自动执行
+__del__析构方法,当前对象在内存中被释放自动斩妖执行
+__call__对象后面加括号触发执行
+__dict__查看类或对象中的成员
+__str__如果一个类中定义了此方法,那么打印此类对象时,输出此方法的返回值 
+__getitem__当类中定义了一个字典的属性成员,可以获取
+__setitem__设置修改类中字典的数据
+__delitem__删除 类中字典的数据
+__metalass__其用来表示该类由 谁 来实例化创建
+__new__触发 __init__创建实例
+__int__将对象强制转换为整数是调用该方法
+__add__两个对象相加的时候会执行—__add__函数，并把第二个对象当成参数传入到__add__函数中
+"""
+class Foo:
+    def __init__(self):
+        print('hello')
+    def __call__(self, *args, **kwargs):
+        print('call')
+    def __int__(self):
+        return 2222
+    def __str__(self):
+        return "str__mmmm"
+    def __add__(self, other):
+        return 888
+
+
+foo = Foo()
+foo()
+
+print(int(foo))
+print(foo)
+str(1)
+
+f1 = Foo()
+f2 = Foo()
+r = f1 + f2
+print(r, type(r))
